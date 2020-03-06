6fa5bbbacb09a518bcd7af4c3eb0ae4d7654fd51
Owen Brasier <owen.brasier@sydney.edu.au>
2020-03-06 16:56:04 +1100

    Added dmx_data to sk_data function

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -8,44 +8,58 @@ LED_2_PIN = machine.Pin(2)
 LED_3_PIN = machine.Pin(3)
 LED_4_PIN = machine.Pin(4)
 
-MY_UNIVERSES = (1, 2, 3)
+_MY_UNIVERSES = (1, 2, 3)
+_NUM_LEDS = const(200)
+_LED_BUFFER_LEN = const(_NUM_LEDS*3)
+_SK_BUFFER_LEN = const(_NUM_LEDS*4)
 
-led1 = neopixel.NeoPixel(LED_1_PIN, 200, bpp=4)
-led2 = neopixel.NeoPixel(LED_2_PIN, 200, bpp=4)
-led3 = neopixel.NeoPixel(LED_3_PIN, 200, bpp=4)
-led4 = neopixel.NeoPixel(LED_4_PIN, 200, bpp=4)
+led1 = neopixel.NeoPixel(LED_1_PIN, _NUM_LEDS, bpp=4)
+led2 = neopixel.NeoPixel(LED_2_PIN, _NUM_LEDS, bpp=4)
+led3 = neopixel.NeoPixel(LED_3_PIN, _NUM_LEDS, bpp=4)
+led4 = neopixel.NeoPixel(LED_4_PIN, _NUM_LEDS, bpp=4)
 
 # sACN vectors
-VECTOR_ROOT_E131_DATA       = (0x00, 0x00, 0x00, 0x04)
-VECTOR_ROOT_E131_EXTENDED   = (0x00, 0x00, 0x00, 0x08)
-VECTOR_E131_DATA_PACKET     = (0x00, 0x00, 0x00, 0x02)
+VECTOR_ROOT_E131_DATA       = const(b'\x00\x00\x00\x04')
+VECTOR_ROOT_E131_EXTENDED   = const(b'\x00\x00\x00\x08')
+VECTOR_E131_DATA_PACKET     = const(b'\x00\x00\x00\x02')
 VECTOR_DMP_SET_PROPERTY     = 0x02
 
-lookup_output = {1: led1, 2: led2, 3: led3}
+lookup_output = {1: (led1, 0), 2: (led2, 0), 3: (led3, 0)}
 
 def is_artnet(packet):
     if packet[:8] != b'Art-Net\0x00':
         return False
     return True
 
-def convert_to_sk(data, universe):
-    # TODO
-    return data, led1
+def get_led_and_offset(universe):
+    return lookup_output[universe]
 
+def update_strip(data, universe):
+    '''less pythonic but should run faster.'''
+    led_strip, offset = get_led_and_offset(universe)
+    pixel = offset
+    for i in range(0, _LED_BUFFER_LEN, 3):
+        led_index = pixel * 4
+        led_strip[led_index] = data[i]
+        led_strip[led_index+1] = data[i+1]
+        led_strip[led_index+2] = data[i+2]
+        led_strip[led_index+3] = 0
+        pixel += 1
+    led_strip.show()
+    
 def handle_artnet(packet):
     op_code, version, sequence, physical, universe, length = unpack('!HHBBHH', packet[8:18])
     if op_code != 0x5000:
         # not OpDmx, ignore
         return
-    if universe not in MY_UNIVERSES:
+    if universe not in _MY_UNIVERSES:
         return
     dmx_data = unpack_from('{}H'.format(length), packet, offset=18)
-    led_data, output = convert_to_sk(dmx_data, universe)
-    output.show()
+    update_strip(dmx_data, universe)
 
 def is_sacn(packet):
-    if tuple(packet[18:22]) != VECTOR_ROOT_E131_DATA or \
-       tuple(packet[40:44]) != VECTOR_E131_DATA_PACKET or \
+    if packet[18:22] != VECTOR_ROOT_E131_DATA or \
+       packet[40:44] != VECTOR_E131_DATA_PACKET or \
        packet[117] != VECTOR_DMP_SET_PROPERTY:
         return False
     return True
@@ -54,14 +68,13 @@ def is_sacn(packet):
 def handle_sacn(packet):
     # TODO 
     priority, addr, sequence, options, universe  = unpack('!BHBBH', packet[108:115])
-    if universe not in MY_UNIVERSES:
+    if universe not in _MY_UNIVERSES:
         return
     preview = (options & 0b10000000) >> 7
     stream_terminated = (options & 0b01000000) >> 6
     force_sync = (options & 0b00100000) >> 5
     dmx_data = unpack('512H', packet[126:638])
-    led_data, output = convert_to_sk(dmx_data, universe)
-
+    update_strip(dmx_data, universe)
 
 while True:
     packet = sock.recv(1144) # 1144 is longest possible sACN packet
@@ -69,9 +82,3 @@ while True:
         handle_artnet(packet)
     elif is_sacn(packet):
         handle_sacn(packet)
-    
-
-
-    
-
-    
