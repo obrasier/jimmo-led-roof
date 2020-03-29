cc84ae4a6c5778f3a21e2243357904b8b964d9cf
Owen Brasier <owen.brasier@sydney.edu.au>
2020-03-29 20:23:40 +1100

    Got ArtNet messages sending and receiving.
    Tested this locally so I believe it will be all good.
    
    Also downscaled the video to 150x9 size - so that should also be
    working. I got numbers that seemed sensible. Need to test this on the
    Raspberry Pi and see how quickly it runs. But looking promising

---
+++ sender/artnet.py
@@ -0,0 +1,93 @@
+import socket
+from struct import pack, unpack, unpack_from
+import time
+
+# ArtPoll packet
+# talk to me information
+# bits 7-5 - 0
+# bit 4 - VLC (visible light communication) transmission
+# we don't support this, set to 1
+# bit 3 - diagnostics broadcast/unicast
+# if they exist they're probs broadcast, set to 0
+# bit 2 - diagnostics true/false
+# true - set to 1
+# bit 1 - ArtPollReply
+# only respond to ArtPoll/ArtAddress packets - 0
+# bit 0 - unused, set to 0
+# self.header.append(0b00010100)
+
+
+class ArtDmxPacket:
+
+    def __init__(self, target_ip='127.0.0.1', universe=0, packet_size=512):
+        self.target_ip = target_ip
+        self.port = 6454
+        self.universe = universe
+        self.subnet = 0
+        self.net = 0
+        self.opcode = 0x5000
+        self.packet_size = packet_size
+        self.header = bytearray()
+        self.buffer = bytearray(self.packet_size)
+        self.sequence = 0x00
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.make_header()
+
+    def make_header(self):
+        self.header = b'Art-Net\x00'
+        version = 0x000e  # version is 14
+        physical = 0x00
+        # universe information
+        # bit 15 - 0
+        # bit 14-8 - Net (1-128)
+        # bit 7-4 - SubNet (1-16)
+        # bit 3-0 - Universe (1-16)
+        # low byte first
+        # a subnet is a group of 16 universes
+        # a net is 16 subnets
+        # we can have 128 nets
+        subnet_universe = (self.subnet << 4 | self.universe) & 0xFF
+        net = self.net & 0x7F  # 7 makes bit 15 = 0
+        
+        self.header += pack('!HHBBBBH', self.opcode, version,
+                            self.sequence, physical, subnet_universe, net, self.packet_size)
+
+    def update(self):
+        packet = bytearray()
+        packet.extend(self.header)
+        if len(self.buffer) > 512:
+            raise Exception('Buffer too long')
+        packet.extend(self.buffer)
+        try:
+            self.socket.sendto(packet, (self.target_ip, self.port))
+        except:
+            print('Error sending packet.')
+
+    def close(self):
+        self.socket.close()
+
+    def set_value(self, address, value):
+        if address > self.packet_size - 1 or address < 0:
+            # out of range
+            return
+        self.buffer[address] = value
+
+    def set_rgb(self, pixel, r, g, b):
+        address = pixel * 3
+        self.set_value(address, r)
+        self.set_value(address+1, g)
+        self.set_value(address+2, b)
+
+    def set_all_rgb(self, r, g, b):
+        for p in range(0, self.packet_size, 3):
+            self.set_rgb(p, r, g, b)
+        self.update()
+
+    def clear(self):
+        self.buffer = bytearray(self.packet_size)
+        self.update()
+
+    def send_nparray(self, np_array):
+        self.buffer = np_array.tobytes()
+        self.update()
+        time.sleep(1)
--- sender/sender.py
+++ sender/sender.py
@@ -1,8 +1,28 @@
 import cv2
 import numpy as np
-import time 
+import time
+import glob
 
-current_ms = lambda: time.time_ns() // 1000000 
+from artnet import ArtDmxPacket
+
+strip_1 = ArtDmxPacket(target_ip='127.0.0.1', universe=1, packet_size=360)
+strip_2 = ArtDmxPacket(target_ip='127.0.0.1', universe=2, packet_size=360)
+strip_3 = ArtDmxPacket(target_ip='127.0.0.1', universe=3, packet_size=360)
+strip_4 = ArtDmxPacket(target_ip='127.0.0.1', universe=4, packet_size=360)
+strip_5 = ArtDmxPacket(target_ip='127.0.0.1', universe=5, packet_size=450)
+strip_6 = ArtDmxPacket(target_ip='127.0.0.1', universe=6, packet_size=450)
+strip_7 = ArtDmxPacket(target_ip='127.0.0.1', universe=7, packet_size=450)
+strip_8 = ArtDmxPacket(target_ip='127.0.0.1', universe=8, packet_size=450)
+strip_9 = ArtDmxPacket(target_ip='127.0.0.1', universe=9, packet_size=450)
+
+LEN_LOOKUP = {0: 120, 1: 120, 2: 120, 3: 120, 4: 150, 5: 150, 6: 150, 7: 150, 8: 150}
+
+LED_STRIPS = [strip_1, strip_2, strip_3, strip_4, strip_5, strip_7, strip_7, strip_8, strip_9]
+
+
+def current_ms():
+    return time.time_ns() // 1000000
+
 
 _NUM_STRIPS = 11
 _STRIP_LENGTH = 200
@@ -12,62 +32,64 @@ dmx_data_start = np.zeros(510, dtype=np.uint8)
 dmx_data_short = np.zeros(300, dtype=np.uint8)
 dmx_data_long = np.zeros(450, dtype=np.uint8)
 
-cap = cv2.VideoCapture("simple_colours.mp4")
-fps = cap.get(cv2.CAP_PROP_FPS)
 
-# get total number of frames
-total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-running_time = fps * total_frames * 30
-
-
-def send_data(dmx_data, strip_num):
-    universe = u_lookup[strip_num]
-
-def process_frame(frame, average=False):
-    if average:
-        process_average_area(frame)
-    strip_num = 0
-    if not average:
-        for y in range(5, 95, 10):
-            # pixels are stored as BGR - this needs to be reflected in the receiver
-            row = np.reshape(frame[y], (frame[y].shape[0]*frame[y].shape[1]))
-            
-
-
+def process_frame(frame, average=False, order_bgr=True):
+    rows, cols, dims = frame.shape
+    if cols != 150 or rows != 9:
+        frame = cv2.resize(frame, dsize=(150, 9), interpolation=cv2.INTER_CUBIC)
+    for strip in range(9):
+        pixel_row = frame[strip]
+        if strip < 4:
+            pixel_row = pixel_row[:120]
+        if order_bgr:
+            row = np.reshape(pixel_row, (pixel_row.shape[0]*pixel_row.shape[1]))
+        else:
+            # swap columns 0 and 2 to get RGB
+            pixel_row[:, [2, 0]] = pixel_row[:, [0, 2]]
+            row = np.reshape(pixel_row, (pixel_row.shape[0]*pixel_row.shape[1]))
+        LED_STRIPS[strip].send_nparray(row)
+        #print(f'{strip}, {len(row)}: {row}')
 
 def get_pixel_rgb(frame, x, y):
     b, g, r = frame[x][y]
     return r, g, b
 
-def process_average_area(frame):
-
-    area = frame[0:50, 0:50]
+def average_area(area):
     if len(area.shape) == 3:
         b, g, r = np.mean(np.mean(area, axis=0), axis=0).astype(np.uint8)
     else:
         b, g, r = np.mean(area, axis=0).astype(np.uint8)
-    
+    return r, g, b
+
 print('starting video')
+videos = glob.glob('videos/*')
+for v in videos:
+    cap = cv2.VideoCapture(v)
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
-_TIME_PER_FRAME_MS = 1000 // fps
+    # get total number of frames
+    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    running_time = fps * total_frames * 30
 
-skip_next_frame = False
+    ms_per_frame = 1000 // fps
 
-for i in range(int(total_frames)):
-    frame_start = current_ms()
-    ret, frame = cap.read()
-    if skip_next_frame:
-        skip_next_frame = False
-        continue
-    row, cols, dims = frame.shape
-    print(total_frames, row, cols, dims)
-    process_frame(frame)
-    frame_end = current_ms()
-    time_left = _TIME_PER_FRAME_MS - (frame_end - frame_start)
-    if time_left > 0:
-        time.sleep(time_left / 1000)
-    else:
-        print("WE ARE GOING TOO SLOW!")
-        skip_next_frame = True
-        
+    skip_next_frame = False
+
+    for i in range(int(total_frames)):
+        frame_start = current_ms()
+        ret, frame = cap.read()
+        if not ret or skip_next_frame:
+            skip_next_frame = False
+            continue
+        row, cols, dims = frame.shape
+        print(total_frames, row, cols, dims)
+        process_frame(frame)
+        frame_end = current_ms()
+        time_left = ms_per_frame - (frame_end - frame_start)
+        if time_left > 0:
+            time.sleep(time_left / 1000)
+        else:
+            print("WE ARE GOING TOO SLOW!")
+            skip_next_frame = True
+
 print('finished video')
