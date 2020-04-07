e9df04f5d71c9b84f20aebda311fe3ee2aee89aa
Owen Brasier <obrasier@gmail.com>
2020-04-07 17:53:06 +1000

    added MAX_FRAMES so it will handle high FPS video

--- sender/sender.py
+++ sender/sender.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 import time
 import glob
-
+from collections import deque
 from artnet import ArtDmxPacket
 
 _SHORT_STRIP_LEDS = 120
 _LONG_STRIP_LEDS = 150
 _VIDEO_WIDTH = _LONG_STRIP_LEDS
 _VIDEO_HEIGHT = 84
+_MAX_FPS = 30
 
 strip_1 = ArtDmxPacket(target_ip='127.0.0.1', universe=1, packet_size=_SHORT_STRIP_LEDS*3)
 strip_2 = ArtDmxPacket(target_ip='127.0.0.1', universe=2, packet_size=_SHORT_STRIP_LEDS*3)
@@ -27,7 +28,7 @@ _NUM_STRIPS = len(LED_STRIPS)
 def current_ms():
     return time.time_ns() // 1000000
 
-def process_frame(frame, average=False, order_rgb=False):
+def process_frame(frame, order_rgb=False):
     rows, cols, dims = frame.shape
     if cols != _VIDEO_WIDTH or rows != _VIDEO_HEIGHT:
         frame = cv2.resize(frame, dsize=(_VIDEO_WIDTH, _VIDEO_HEIGHT), interpolation=cv2.INTER_CUBIC)
@@ -38,30 +39,52 @@ def process_frame(frame, average=False, order_rgb=False):
         if order_rgb:
             # swap columns 0 and 2 to convert BGR -> RGB
             pixel_row[:, [2, 0]] = pixel_row[:, [0, 2]]
-        row = np.reshape(pixel_row, (pixel_row.shape[0]*pixel_row.shape[1]))
-        LED_STRIPS[strip].send_nparray(row)
+        dmx_data = np.reshape(pixel_row, (pixel_row.shape[0]*pixel_row.shape[1]))
+        LED_STRIPS[strip].send_nparray(dmx_data)
         #print(f'{strip}, {len(row)}: {row}')
-
 
 print('starting video')
 videos = glob.glob('videos/*')
 for v in videos:
     cap = cv2.VideoCapture(v)
     fps = cap.get(cv2.CAP_PROP_FPS)
-
-    # get total number of frames
-    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-
-    ms_per_frame = 1000 // fps
-
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    # if the video is too high FPS, get the frames we will use and
+    # put them in a deque, so we can pop off the frames we need
+    if fps > _MAX_FPS:
+        frames_used = deque()
+        duration = total_frames / fps
+        for n in np.linspace(0, total_frames, _MAX_FPS * duration, dtype=int):
+            frames_used.appendleft(n)
+        skipping_frames = True
+        ms_per_frame = 1000 // _MAX_FPS
+    else:
+        ms_per_frame = 1000 // fps
+        skipping_frames = False
     skip_next_frame = False
 
-    for i in range(int(total_frames)):
-        frame_start = current_ms()
-        ret, frame = cap.read()
-        if not ret or skip_next_frame:
+    if skipping_frames:
+        next_frame = frames_used.pop()
+
+    for frame_num in range(total_frames):
+        # if the FPS is too high, check if we need to skip this frame
+        if skipping_frames: 
+            if frame_num == next_frame:
+                next_frame = frames_used.pop()
+            else:
+                continue
+        
+        if skip_next_frame:
             skip_next_frame = False
             continue
+
+        frame_start = current_ms()
+        ret, frame = cap.read()
+        if not ret:
+            # return failed, no frame read, skip
+            continue
+
         row, cols, dims = frame.shape
         print(total_frames, row, cols, dims)
         process_frame(frame)
